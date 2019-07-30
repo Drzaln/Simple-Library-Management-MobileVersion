ba73862e5c564aae66bbd565d785ca5d0898610d
Drzaln <drzaln@null.net>
2019-07-30 09:49:28 +0700

    Login Screen

--- src/public/navigators/MainNavigator.js
+++ src/public/navigators/MainNavigator.js
@@ -2,10 +2,12 @@ import { createStackNavigator, createAppContainer } from "react-navigation";
 
 import Home from "../../screens/Home";
 import Login from "../../screens/Login";
+import Register from "../../screens/Register";
 
 const AppNavigator = createStackNavigator({
     Home,
     Login,
+    // Register,
 }, {
     headerMode:'none',
     initialRouteName:'Login'
