ba37a61c28c21befaa27b866b8c1c1d9d988d456
Drzaln <drzaln@null.net>
2019-07-30 11:53:14 +0700

    add icon launcher

--- src/public/navigators/MainNavigator.js
+++ src/public/navigators/MainNavigator.js
@@ -3,6 +3,7 @@ import { createStackNavigator, createAppContainer } from "react-navigation";
 import Home from "../../screens/Home";
 import Login from "../../screens/Login";
 import Register from "../../screens/Register";
+import DetailBook from "../../screens/DetailBook";
 
 const AppNavigator = createStackNavigator({
     Home,
